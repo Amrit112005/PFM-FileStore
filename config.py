dcd75fa6e8af2f7c71f93deae5b12eba74e5c647
Amrit112005 <amrit2005pay@gmail.com>
2025-01-03 13:21:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -65,7 +65,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(5733685945)
+ADMINS.append(5094191566)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
